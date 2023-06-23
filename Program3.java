b65fb2d5b941c7d41418e22e8d142615d331d4dd
Evgeniy <andrej.burakov.91@mail.ru>
2023-06-23 19:03:07 +0300

    pr3

---
+++ Program3.java
@@ -0,0 +1,13 @@
+package hw7;
+
+
+public class Program3 {
+
+    public static void main(String[] args) {
+        Settings settings = Settings.getInstance();
+
+        //Settings settings1 = new Settings();
+        //Settings settings2 = new Settings();
+    }
+
+}
